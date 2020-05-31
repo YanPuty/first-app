9370ff30424953903c6919029a411ae2d07c814c
yanputy <puty.yan@sparkdigital.asia>
2020-05-31 13:59:59 +0700

    test with lambda function

--- app/src/main/java/com/example/myapplication/HelloWorld.kt
+++ app/src/main/java/com/example/myapplication/HelloWorld.kt
@@ -8,4 +8,13 @@ fun main() {
     var fullName: String
     fullName = "Dexter X$isAwesome"
     println(fullName)
+
+    lambdaFunction ("asd") { valueA, valueB ->
+        val sum = valueA + valueB
+        println("The sum is: $sum")
+    }
+}
+
+fun lambdaFunction (asd: String, completion: (valueA: Int, valueB: Int) -> Unit) {
+    completion(2 ,3)
 }
